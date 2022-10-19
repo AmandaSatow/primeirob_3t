19236698e24dd965d52ca59e1687c5222035ea25
AmandaSatow <108934778+AmandaSatow@users.noreply.github.com>
2022-10-19 12:47:37 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -47,3 +47,43 @@ function total(){
     
     document.write("Resultado: " + r );
 }
+
+function media(){
+    let n1 = document.getElementById("n1").value;
+    let n2 = document.getElementById("n2").value;
+    let n3 = document.getElementById("n3").value;
+    let n4 = document.getElementById("n4").value;
+
+    let r = (Number(n1)+Number(n2)+Number(n3)+Number(n4))/4;
+    document.getElementById("resultado").innerHTML = "Média: " + r;
+}
+
+function soma(){
+    let n1 = document.getElementById("n1").value;
+    let n2 = document.getElementById("n2").value;
+    let n3 = document.getElementById("n3").value;
+    let n4 = document.getElementById("n4").value;
+
+    let r = (Number(n1)+Number(n2)+Number(n3)+Number(n4))
+    document.getElementById("resultado").innerHTML = "Soma: " + r;
+}
+
+function subtrai(){
+    let n1 = document.getElementById("n1").value;
+    let n2 = document.getElementById("n2").value;
+    let n3 = document.getElementById("n3").value;
+    let n4 = document.getElementById("n4").value;
+
+    let r = (Number(n1)-Number(n2)-Number(n3)-Number(n4))
+    document.getElementById("resultado").innerHTML = "Subtração: " + r;
+}
+
+function multiplica(){
+    let n1 = document.getElementById("n1").value;
+    let n2 = document.getElementById("n2").value;
+    let n3 = document.getElementById("n3").value;
+    let n4 = document.getElementById("n4").value;
+
+    let r = (Number(n1)*Number(n2)*Number(n3)*Number(n4))
+    document.getElementById("resultado").innerHTML = "Multiplicação: " + r;
+}
